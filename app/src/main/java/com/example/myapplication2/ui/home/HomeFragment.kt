5bdc5d9d2c8aabaea9b6855801ce07ab1910de46
RiquelSantos <riquesantosoliveira@gmail.com>
2025-10-03 15:04:53 -0300

    feat: Adiciona um novo layout mais intuitivo para a tela de Home

--- app/src/main/java/com/example/myapplication2/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication2/ui/home/HomeFragment.kt
@@ -5,7 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.myapplication2.R
 import com.example.myapplication2.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -18,15 +19,24 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            binding.textHome.text = it
+        // Removemos a ViewModel e o text_home, que agora é text_home_title no XML estático
+
+        // Configurar a navegação dos botões
+        binding.btnNavigateEscolas.setOnClickListener {
+            findNavController().navigate(R.id.nav_escola)
         }
+
+        binding.btnNavigateAlunos.setOnClickListener {
+            findNavController().navigate(R.id.nav_alunos)
+        }
+
+        binding.btnNavigateEquipes.setOnClickListener {
+            findNavController().navigate(R.id.nav_equipe)
+        }
+
         return root
     }
 
